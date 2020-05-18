9d48b04326a721957dcff831034ad75c33ec452c
mateus <mateuspaivamatiazzi1@gmail.com>
2020-05-18 00:31:58 -0400

    Front adicionando um botão para mostrar todos os funcionários no banco de dados

---
+++ funcionario-vue/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <button @click="mostrarFuncionarios()">Mostrar Funcionarios</button>
+    <div v-if="info">
+      <p>{{info}}</p>
+    </div>  
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      info: ''
+    }
+  },
+  methods: {
+    async mostrarFuncionarios(){
+      const info = await axios.get("http://localhost:8080/api/funcionarios/todos");
+      this.info = info
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
